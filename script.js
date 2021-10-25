fa6e9d749a1d5449ac7b9e5d36723f640c5817e8
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-10-25 16:32:01 +0200

    first working solution

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ const squareContainer = document.getElementById("container");
 
 for (let i = 1; i <= 100; i++) {
     const squareBox = `<div class="square">${i}</div>`;
-    squareContainer.innerHTML += squareBox;
+    const squareBox3 = `<div class="square square_3">Fizz</div>`;
+    const squareBox5 = `<div class="square square_5">Buzz</div>`;
+    const squareBox15 = `<div class="square square_15">FizzBuzz</div>`;
+
+    if (i % 3 !== 0 && i % 5 !== 0) {
+        squareContainer.innerHTML += squareBox;
+    } else if (i % 3 == 0 && i % 5 !== 0) {
+        squareContainer.innerHTML += squareBox3;
+    } else if (i % 5 == 0 && i % 3 !== 0) {
+        squareContainer.innerHTML += squareBox5;
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        squareContainer.innerHTML += squareBox15;
+    }
 }
